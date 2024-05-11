143f25e2c01c503e05de3d294a28e63d87096bb7
AkhilaBejagam <akhila.bejagam@jukshio.com>
2024-05-11 19:25:28 +0530

    add new code

--- print10numbers.py
+++ print10numbers.py
@@ -1,6 +1,13 @@
-num = list(range(10))
-previousNum = 0
-for i in num:
-    sum = previousNum + i
-    print('Current Number '+ str(i) + 'Previous Number' + str(previousNum) + 'is ' + str(sum))
-    previousNum= i
+import argparse
+c = argparse.ArgumentParser(description='process some integers')
+
+c.add_argument('current_number', type=int)
+c.add_argument('previous_number', type=int)
+args = c.parse_args()
+current_num = args.current_number
+previous_num = args.previous_number
+
+for i in range(10):
+    sum = previous_num + i
+    print(f'Current number {i} Previous Number {previous_num} is {sum}')
+    previous_num = i
